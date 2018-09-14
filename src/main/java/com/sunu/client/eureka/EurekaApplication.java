dd44b7a84aa8eb1a0e02ead5d533d93979966fdc
Unknown <sunu@wemakeprice.com>
2018-09-14 11:23:15 +0900

    master : 소스

--- src/main/java/com/sunu/client/eureka/EurekaApplication.java
+++ src/main/java/com/sunu/client/eureka/EurekaApplication.java
@@ -11,6 +11,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
+
+
 @EnableDiscoveryClient
 @SpringBootApplication
 public class EurekaApplication {
